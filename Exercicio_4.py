ca64f5f65e3423da7018886d9448425c6980c3c7
Pedro-Aluno <pedrohpires2023@gmail.com>
2026-03-30 20:49:24 -0300

    Exercicio 4 Folha 3

---
+++ Exercicio_4.py
@@ -0,0 +1,22 @@
+total_arrecadado = 0
+
+for i in range(10):
+ print(f"\nCliente {i+10}")
+ 
+nome = input("Digite o nome do cliente: ")
+valor_compra = float(input("Digite o valor da compra: "))
+
+if valor_compra >= 250:
+    desconto = valor_compra * 0.20
+else:
+    desconto = valor_compra * 0.15
+    
+valor_pagar = valor_compra - desconto
+
+total_arrecadado += valor_pagar
+print("Nome", nome)
+print("Valor da compra: $", valor_compra)
+print("Valor do desconto: $", desconto)
+print("Valor a pagar cada cliente: $", valor_pagar)
+
+print("\nTotal arrecadado pela loja: $", total_arrecadado)   
